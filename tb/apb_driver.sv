0e32fd965d6221740fabcd451bae65195f8ccd3e
TanmayKalkur <tanmay.kalkur@gmail.com>
2024-10-24 14:27:39 +0530

    updated driver

--- tb/apb_driver.sv
+++ tb/apb_driver.sv
@@ -1,4 +1,4 @@
-
+`define DRV_if vif.DRV.drv_cb
 class apb_driver extends uvm_driver#(apb_seq_item);
   
   //------register with the uvm factory ---------
@@ -29,9 +29,41 @@ class apb_driver extends uvm_driver#(apb_seq_item);
   endtask
 
   task drive();
-    
-
+    if(vif.presetn == 0) begin
+      @(posedge pclk);
+      `DRV_if.i_paddr <= 'bz;
+      `DRV_if.i_pwrite <= 'bz;
+      `DRV_if.i_psel <= 'bz;
+      `DRV_if.i_penable <= 'bz;
+      `DRV_if.i_pwdata <= 'bz;
+      `DRV_if.i_pstrb <= 'bz;
+    end
+    else begin
+      @(posedge pclk);
+      if(req.i_pwrite == 1) begin
+        write();
+      end
+      else begin
+        read();
+      end
+    end
   endtask
+
+  //-----write operation-------
+
+  task write();
+    @(posedge pclk);
+    `DRV_if.i_psel <= 1;
+    `DRV_if.i_paddr <= req.i_paddr;
+    `DRV_if.i_pwdata <= req.i_pwdata;
+    @(posedge pclk);
+    `DRV_if.i_penable <= 1;
+    @(posedge pclk);
+
+    `DRV_if.i_penable <= 1;
+    `DRV_if.i_psel <= 1;
+
+
 
 endclass: apb_driver
 
