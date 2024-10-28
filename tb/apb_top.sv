65d671a3954be974ba2388cf17d343749c0acba0
TanmayKalkur <tanmay.kalkur@gmail.com>
2024-10-28 10:33:17 +0530

    updated top

--- tb/apb_top.sv
+++ tb/apb_top.sv
@@ -14,9 +14,33 @@ module top();
     presetn = 1;
   end
 
-  apb_intrf(pclk, presetn);
+  apb_intrf inf(pclk, presetn);
 
   //-----instantiate design---------
 
-  
+  apb_slave (32,5) dut(
+   .pclk(inf.pclk),    
+   .presetn(inf.presetn), 
+   .i_paddr(inf.i_paddr),   
+   .i_pwrite(inf.i_pwrite), 
+   .i_psel(inf.i_psel),   
+   .i_penable(inf.i_penable),
+   .i_pwdata(inf.i_pwdata), 
+   .i_pstrb(inf.i_pstrb),  
+   .o_prdata(inf.o_prdata), 
+   .o_pslverr(inf.o_pslverr),
+   .o_pready(inf.o_pready), 
+
+   .o_hw_ctl(inf.o_hw_ctl), 
+   .i_hw_sts(inf.i_hw_sts) 
+  );
+
+  initial begin
+    uvm_config_db #(virtual apb_intrf)::set(null,"*","vif",inf);
+  end
+
+  intial begin
+    run_test();
+  end
+
 
